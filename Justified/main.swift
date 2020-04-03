b8935660b1f0ad6cb5c833da708de0d07b59418f
MARK KUHARICH <kuharich@hotmail.com>
2020-04-03 14:34:45 -0700

    Initial Commit

---
+++ Justified/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Justified
+//
+//  Created by Mark Kuharich on 4/3/20.
+//  Copyright © 2020 Mark Kuharich. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
